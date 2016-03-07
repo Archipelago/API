2aeb15f25e6b24eebe4d60ce839c9f11d673420c
Niovad <niovad@safe-mail.net>
2016-03-07 09:57:00 +0000

    Add route for login, that returns a token
        Fix salt generation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+var crypto = require('crypto');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
@@ -10,6 +11,8 @@ GLOBAL.db = new Maria({host: config.db.host,
 		       user: config.db.user,
 		       password: config.db.password,
 		       db: config.db.name});
+GLOBAL.tokens = {};
+GLOBAL.users = {};
 
 app.listen(process.env.PORT || config.port);
 app.use(bodyParser.json({type: '*/*'}));
@@ -19,5 +22,24 @@ app.post('/register', function(req, res) {
       sendResponse(res, 400, {status: "Error", message: e});
     else
       sendResponse(res, 201, {status: "Created"});
+  });
+});
+
+app.post('/login', function(req, res) {
+  require('./models/user.js').login(req.body, function(e, r) {
+    if (e)
+      sendResponse(res, 400, {status: "Error", message: e});
+    else {
+      var login = req.body.login.trim();
+      if (users[login]
+	  && users[login].token)
+	sendResponse(res, 200, {Status: "OK", Token: users[login].token});
+      else {
+	var t = crypto.randomBytes(32).toString('hex');
+	users[login] = {token: t};
+	tokens[t] = login
+	sendResponse(res, 200, {Status: "OK", Token: t});
+      }
+    }
   });
 });
--- models/user.js
+++ models/user.js
@@ -20,11 +20,31 @@ module.exports.register = function(infos, cb) {
   else {
     if (infos.bm && infos.bm.substr(0, 3) !== 'BM-')
       infos.bm = 'BM-' + infos.bm;
-    infos.salt = crypto.randomBytes(32).toString('hex');
+    infos.salt = crypto.randomBytes(16).toString('hex');
     infos.password = crypto.createHmac('sha256', infos.salt + infos.password).digest('hex');
     db.query('INSERT INTO `Users`(`login`, `salt`, `password`, `mail`, `bm`) VALUES(:login, :salt, :password, :mail, :bm)', infos, function(e, r) {
       if (e && e.code == 1062)
 	cb('User "' + infos.login + '" already exists');
+      else
+	cb(e, r);
+    });
+  }
+}
+
+module.exports.login = function(infos, cb) {
+  for (i in infos)
+    infos[i] = infos[i].trim();
+
+  if (infos.login === undefined)
+    cb('Login must be provided');
+  else if (infos.password === undefined)
+    cb('Password must be provided');
+  else {
+    db.query('SELECT `salt`, `password` FROM `Users` WHERE `login` = ?', [infos.login], function(e, r) {
+      if (r.info.numRows != 1)
+	cb('User "' + infos.login + '" not found');
+      else if (crypto.createHmac('sha256', r[0].salt + infos.password).digest('hex') != r[0].password)
+	cb('Invalid password');
       else
 	cb(e, r);
     });
