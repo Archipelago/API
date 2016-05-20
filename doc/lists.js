7a3c670a7591aa0a56f13fc00c0432df085118d0
Niovad <niovad@safe-mail.net>
2016-05-21 01:12:06 +0200

    Add doc about future list/* routes
        Those routes are used to list qualities, containers, sources,
        audio and video codecs, languages and compressions available
        when creating new releases

---
+++ doc/lists.js
@@ -0,0 +1,124 @@
+/**
+ * @api {get} /list/audioCodecs Audio codecs
+ * @apiName ListAudioCodecs
+ * @apiGroup List
+ * @apiDescription Get list of available audio codecs
+ * @apiPermission logged
+ * @apiPermission unlogged
+ *
+ * @apiSuccess (200) {Array} data
+ * @apiSuccessExample Success-Response
+ *     HTTP/1.1 200 OK
+ *     [
+ *       "mp3",
+ *       "AC3",
+ *       "DD"
+ *     ]
+ */
+
+/**
+ * @api {get} /list/videoCodecs Video codecs
+ * @apiName ListVideoCodecs
+ * @apiGroup List
+ * @apiDescription Get list of available video codecs
+ * @apiPermission logged
+ * @apiPermission unlogged
+ *
+ * @apiSuccess (200) {Array} data
+ * @apiSuccessExample Success-Response
+ *     HTTP/1.1 200 OK
+ *     [
+ *       "XviD",
+ *       "x264",
+ *       "x265"
+ *     ]
+ */
+
+/**
+ * @api {get} /list/sources Sources
+ * @apiName Listources
+ * @apiGroup List
+ * @apiDescription Get list of available sources
+ * @apiPermission logged
+ * @apiPermission unlogged
+ *
+ * @apiSuccess (200) {Array} data
+ * @apiSuccessExample Success-Response
+ *     HTTP/1.1 200 OK
+ *     [
+ *       "DVDRiP",
+ *       "BDRiP",
+ *       "Web-dl"
+ *     ]
+ */
+
+/**
+ * @api {get} /list/qualities Qualities
+ * @apiName ListQualities
+ * @apiGroup List
+ * @apiDescription Get list of available qualities
+ * @apiPermission logged
+ * @apiPermission unlogged
+ *
+ * @apiSuccess (200) {Array} data
+ * @apiSuccessExample Success-Response
+ *     HTTP/1.1 200 OK
+ *     [
+ *       "SD",
+ *       "720p",
+ *       "1080p"
+ *     ]
+ */
+
+/**
+ * @api {get} /list/qualities Containers
+ * @apiName ListContainers
+ * @apiGroup List
+ * @apiDescription Get list of available containers
+ * @apiPermission logged
+ * @apiPermission unlogged
+ *
+ * @apiSuccess (200) {Array} data
+ * @apiSuccessExample Success-Response
+ *     HTTP/1.1 200 OK
+ *     [
+ *       "avi",
+ *       "mp4",
+ *       "mkv"
+ *     ]
+ */
+
+/**
+ * @api {get} /list/languages Languages
+ * @apiName ListLanguages
+ * @apiGroup List
+ * @apiDescription Get list of available languages
+ * @apiPermission logged
+ * @apiPermission unlogged
+ *
+ * @apiSuccess (200) {Array} data
+ * @apiSuccessExample Success-Response
+ *     HTTP/1.1 200 OK
+ *     [
+ *       "en",
+ *       "ru",
+ *       "fr"
+ *     ]
+ */
+
+/**
+ * @api {get} /list/compressions Compressions
+ * @apiName ListCompressions
+ * @apiGroup List
+ * @apiDescription Get list of available compressions
+ * @apiPermission logged
+ * @apiPermission unlogged
+ *
+ * @apiSuccess (200) {Array} data
+ * @apiSuccessExample Success-Response
+ *     HTTP/1.1 200 OK
+ *     [
+ *       "rar",
+ *       "zip"
+ *     ]
+ */
