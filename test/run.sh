68565fe72be205308b5f3c8b459b7aff292b4597
Niovad <niovad@safe-mail.net>
2016-05-29 19:27:10 +0200

    Add tests for /login route and for tokens check

--- test/run.sh
+++ test/run.sh
@@ -2,7 +2,7 @@
 
 ret=0
 cd `dirname $0`
-npm start & sleep 1
+npm start & sleep 0.5
 nodeunit init.js user.js || ret=1
 kill %1
 exit $ret
--- test/user.js
+++ test/user.js
@@ -22,3 +22,64 @@ exports.register = {
     });
   }
 }
+
+exports.login = {
+  erroneousPassword: function(test) {
+    request('/login', {
+      "login": "tmp" + hash,
+      "password": "foobar41"
+    }, function(res) {
+      test.equal(res.statusCode, 400);
+      test.done();
+    });
+  },
+
+  noPassword: function(test) {
+    request('/login', {
+      "login": "tmp" + hash
+    }, function(res) {
+      test.equal(res.statusCode, 400);
+      test.done();
+    });
+  },
+
+  valid: function(test) {
+    request('/login', {
+      "login": "tmp" + hash,
+      "password": "foobar42"
+    }, function(res) {
+      global.token = res.body.token;
+      test.equal(res.statusCode, 200);
+      test.done();
+    });
+  }
+}
+
+exports.token = {
+  sameToken: function(test) {
+    request('/login', {
+      "login": "tmp" + hash,
+      "password": "foobar42"
+    }, function(res) {
+      test.equal(global.token, res.body.token);
+      test.done();
+    });
+  },
+
+  differentToken: function(test) {
+    let hash = crypto.randomBytes(4).toString('hex');
+    request('/register', {
+      "login": "tmp" + hash,
+      "password": "foobar42"
+    }, function(res) {
+      test.equal(res.statusCode, 201);
+      request('/login', {
+	"login": "tmp" + hash,
+	"password": "foobar42"
+      }, function(res) {
+	test.notEqual(global.token, res.body.token);
+	test.done();
+      });
+    });
+  }
+}
