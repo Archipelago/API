f9e0bcb0e985f1dbcb6f44daba73655d2c3bb151
Niovad <niovad@safe-mail.net>
2016-06-06 11:17:16 +0200

    Add doc for future route /search

---
+++ doc/search.js
@@ -0,0 +1,47 @@
+/**
+ * @api {get} /search Search elements
+ * @apiVersion 0.1.0
+ * @apiName Search
+ * @apiGroup Search
+ * @apiDescription Search for movies, releases, links and users
+ * @apiPermission logged
+ * @apiPermission unlogged
+ *
+ * @apiParam {String{3..}} query The terms of the query
+ * @apiParam {String} q A shortcut for `query`
+ * @apiParam {String...="movie","release","link","user","*"} [type="*"] The maximum number of rows retrieved
+ * @apiParam {String...} [t] A shortcut for `type`
+ *
+ * @apiParamExample {querystring} Request-Example
+ *     /search?query=foobar&t=movie,user
+ *
+ * @apiSuccess (200) {Object} results
+ * @apiSuccessExample Success-Response
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "movie": [
+ *         {
+ *           "id": 1,
+ *           "title": "foobar"
+ *         }
+ *       ],
+ *       "release": [
+ *         {
+ *           "id": 1,
+ *           "name": "foobar.en.720p.AC3.x264"
+ *         }
+ *       ],
+ *       "link": [
+ *           "https://example.com/release.mkv"
+ *       ],
+ *       "user": [
+ *         {
+ *           "id": 1,
+ *           "login": "foobar"
+ *         }
+ *       ]
+ *     }
+ *
+ * @apiUse MissingParameter
+ * @apiUse InvalidParameter
+ */
