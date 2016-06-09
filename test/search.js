f6b33a593f9001d5ea09e6f30b554363371094b0
Niovad <niovad@safe-mail.net>
2016-06-09 11:06:11 +0200

    Add tests for return values of /search route

--- test/search.js
+++ test/search.js
@@ -49,3 +49,75 @@ exports.parameters = {
     }
   }
 };
+
+exports.searchedValues = {
+  movie: function(test) {
+    request('/search?query=foo%20bar&t=movie', function(res) {
+      test.equal(res.statusCode, 200);
+      test.equal(_.size(res.body), 1);
+      test.equal(res.body.movie instanceof Array, true);
+      test.done();
+    });
+  },
+
+  user: function(test) {
+    request('/search?query=foo%20bar&t=user', function(res) {
+      test.equal(res.statusCode, 200);
+      test.equal(_.size(res.body), 1);
+      test.equal(res.body.user instanceof Array, true);
+      test.done();
+    });
+  },
+
+  release: function(test) {
+    request('/search?query=foo%20bar&t=release', function(res) {
+      test.equal(res.statusCode, 200);
+      test.equal(_.size(res.body), 1);
+      test.equal(res.body.release instanceof Array, true);
+      test.done();
+    });
+  },
+
+  link: function(test) {
+    request('/search?query=foo%20bar&t=link', function(res) {
+      test.equal(res.statusCode, 200);
+      test.equal(_.size(res.body), 1);
+      test.equal(res.body.link instanceof Array, true);
+      test.done();
+    });
+  },
+
+  several: function(test) {
+    request('/search?query=foo%20bar&t=movie,user', function(res) {
+      test.equal(res.statusCode, 200);
+      test.equal(_.size(res.body), 2);
+      test.equal(res.body.movie instanceof Array, true);
+      test.equal(res.body.user instanceof Array, true);
+      test.done();
+    });
+  },
+
+  wildcard: function(test) {
+    request('/search?query=foo%20bar&t=*', function(res) {
+      test.equal(res.statusCode, 200);
+      test.equal(_.size(res.body), 4);
+      test.equal(res.body.movie instanceof Array, true);
+      test.equal(res.body.user instanceof Array, true);
+      test.equal(res.body.release instanceof Array, true);
+      test.equal(res.body.link instanceof Array, true);
+      test.done();
+    });
+  },
+
+  default: function(test) {
+    request('/search?query=foo%20bar', function(res) {
+      test.equal(res.statusCode, 200);
+      test.equal(_.size(res.body), 4);
+      test.equal(res.body.movie instanceof Array, true);
+      test.equal(res.body.user instanceof Array, true);
+      test.equal(res.body.release instanceof Array, true);
+      test.equal(res.body.link instanceof Array, true);
+      test.done();
+    });
+  }
+}
