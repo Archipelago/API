e1350cb8e69d090544fb8dde20915d668bbaaf04
Niovad <niovad@safe-mail.net>
2016-07-11 09:51:53 +0200

    Update binary values of permissions

--- token.js
+++ token.js
@@ -7,19 +7,19 @@ module.exports = function() {
   // login: {token, id, permissions}
   let users = {};
   const permissions = {
-    'ADD_ELEMENT': 1 << 1,
-    'EDIT_ELEMENT': 1 << 2,
-    'DELETE_ELEMENT': 1 << 3,
-    'ADD_RELEASE': 1 << 4,
-    'EDIT_RELEASE': 1 << 5,
-    'DELETE_RELEASE': 1 << 6,
-    'ADD_LINK': 1 << 7,
-    'EDIT_LINK': 1 << 8,
-    'DELETE_LINK': 1 << 9,
-    'EDIT_USER': 1 << 10,
-    'DELETE_USER': 1 << 11,
-    'BAN_USER': 1 << 12,
-    'EDIT_PERMISSION': 1 << 13
+    'ADD_ELEMENT': 1 << 0,
+    'EDIT_ELEMENT': 1 << 1,
+    'DELETE_ELEMENT': 1 << 2,
+    'ADD_RELEASE': 1 << 3,
+    'EDIT_RELEASE': 1 << 4,
+    'DELETE_RELEASE': 1 << 5,
+    'ADD_LINK': 1 << 6,
+    'EDIT_LINK': 1 << 7,
+    'DELETE_LINK': 1 << 8,
+    'EDIT_USER': 1 << 9,
+    'DELETE_USER': 1 << 10,
+    'BAN_USER': 1 << 11,
+    'EDIT_PERMISSION': 1 << 12
   };
 
   this.getId = function(t) {
