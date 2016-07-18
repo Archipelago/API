617fd592ee40afdf69eb5ee029815083c87c93f8
Niovad <niovad@safe-mail.net>
2016-07-18 23:05:19 +0200

    Add tests for permissions removal

--- test/permission.js
+++ test/permission.js
@@ -1,5 +1,5 @@
 // Tests are done with id 2
-// We need to add a serie of users and to retrieve their ids
+// TODO: We need to add a serie of users and to retrieve their ids
 
 exports.grant = {
   unlogged: function(test) {
@@ -50,6 +50,60 @@ exports.grant = {
     request.get('/user/2', function(res) {
       test.equal(res.statusCode, 200);
       test.deepEqual(res.body.permissions, ["ADD_ELEMENT", "EDIT_ELEMENT", "DELETE_ELEMENT"]);
+      test.done();
+    });
+  }
+}
+
+exports.revoke = {
+  unlogged: function(test) {
+    request.put('/user/2/permission', {
+      "remove": [
+	"DELETE_ELEMENT"
+      ]
+    }, function(res) {
+      test.equal(res.statusCode, 401);
+      test.done();
+    });
+  },
+
+  unauthorized: function(test) {
+    request.put('/user/2/permission', global.token, {
+      "remove": [
+	"DELETE_ELEMENT"
+      ]
+    }, function(res) {
+      test.equal(res.statusCode, 403);
+      test.done();
+    });
+  },
+
+  ownpermissions: function(test) {
+    request.put('/user/1/permission', global.rootToken, {
+      "remove": [
+	"DELETE_ELEMENT"
+      ]
+    }, function(res) {
+      test.equal(res.statusCode, 403);
+      test.done();
+    });
+  },
+
+  rootUser: function(test) {
+    request.put('/user/2/permission', global.rootToken, {
+      "remove": [
+	"DELETE_ELEMENT"
+      ]
+    }, function(res) {
+      test.equal(res.statusCode, 200);
+      test.done();
+    });
+  },
+
+  checkUpdatedPermissions: function(test) {
+    request.get('/user/2', function(res) {
+      test.equal(res.statusCode, 200);
+      test.deepEqual(res.body.permissions, ["ADD_ELEMENT", "EDIT_ELEMENT"]);
       test.done();
     });
   }
