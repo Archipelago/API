b7c027bd543d447c8fb90e27032c42eb11695bc9
Niovad <niovad@safe-mail.net>
2016-12-10 17:24:33 +0000

    Add tests for missing parameters for a release

--- test/release.js
+++ test/release.js
@@ -35,6 +35,128 @@ exports.add = {
     });
   },
 
+  missingField: {
+    name: function(test) {
+      request.post('/movie/1/release', global.rootToken, {
+	"size": "2.1GB",
+	"language": randomElem(global.lists.languages),
+	"audio_codec": randomElem(global.lists.audioCodecs),
+	"video_codec": randomElem(global.lists.videoCodecs),
+	"source": randomElem(global.lists.sources),
+	"quality": randomElem(global.lists.qualities),
+	"container": randomElem(global.lists.containers)
+      }, function(res) {
+	test.equal(res.statusCode, 400);
+	test.done();
+      });
+    },
+
+    size: function(test) {
+      request.post('/movie/1/release', global.rootToken, {
+	"name": "foobar",
+	"language": randomElem(global.lists.languages),
+	"audio_codec": randomElem(global.lists.audioCodecs),
+	"video_codec": randomElem(global.lists.videoCodecs),
+	"source": randomElem(global.lists.sources),
+	"quality": randomElem(global.lists.qualities),
+	"container": randomElem(global.lists.containers)
+      }, function(res) {
+	test.equal(res.statusCode, 400);
+	test.done();
+      });
+    },
+
+    language: function(test) {
+      request.post('/movie/1/release', global.rootToken, {
+	"name": "foobar",
+	"size": "2.1GB",
+	"audio_codec": randomElem(global.lists.audioCodecs),
+	"video_codec": randomElem(global.lists.videoCodecs),
+	"source": randomElem(global.lists.sources),
+	"quality": randomElem(global.lists.qualities),
+	"container": randomElem(global.lists.containers)
+      }, function(res) {
+	test.equal(res.statusCode, 400);
+	test.done();
+      });
+    },
+
+    audioCodec: function(test) {
+      request.post('/movie/1/release', global.rootToken, {
+	"name": "foobar",
+	"size": "2.1GB",
+	"language": randomElem(global.lists.languages),
+	"video_codec": randomElem(global.lists.videoCodecs),
+	"source": randomElem(global.lists.sources),
+	"quality": randomElem(global.lists.qualities),
+	"container": randomElem(global.lists.containers)
+      }, function(res) {
+	test.equal(res.statusCode, 400);
+	test.done();
+      });
+    },
+
+    videoCodec: function(test) {
+      request.post('/movie/1/release', global.rootToken, {
+	"name": "foobar",
+	"size": "2.1GB",
+	"language": randomElem(global.lists.languages),
+	"audio_codec": randomElem(global.lists.audioCodecs),
+	"source": randomElem(global.lists.sources),
+	"quality": randomElem(global.lists.qualities),
+	"container": randomElem(global.lists.containers)
+      }, function(res) {
+	test.equal(res.statusCode, 400);
+	test.done();
+      });
+    },
+
+    source: function(test) {
+      request.post('/movie/1/release', global.rootToken, {
+	"name": "foobar",
+	"size": "2.1GB",
+	"language": randomElem(global.lists.languages),
+	"audio_codec": randomElem(global.lists.audioCodecs),
+	"video_codec": randomElem(global.lists.videoCodecs),
+	"quality": randomElem(global.lists.qualities),
+	"container": randomElem(global.lists.containers)
+      }, function(res) {
+	test.equal(res.statusCode, 400);
+	test.done();
+      });
+    },
+
+    quality: function(test) {
+      request.post('/movie/1/release', global.rootToken, {
+	"name": "foobar",
+	"size": "2.1GB",
+	"language": randomElem(global.lists.languages),
+	"audio_codec": randomElem(global.lists.audioCodecs),
+	"video_codec": randomElem(global.lists.videoCodecs),
+	"source": randomElem(global.lists.sources),
+	"container": randomElem(global.lists.containers)
+      }, function(res) {
+	test.equal(res.statusCode, 400);
+	test.done();
+      });
+    },
+
+    container: function(test) {
+      request.post('/movie/1/release', global.rootToken, {
+	"name": "foobar",
+	"size": "2.1GB",
+	"language": randomElem(global.lists.languages),
+	"audio_codec": randomElem(global.lists.audioCodecs),
+	"video_codec": randomElem(global.lists.videoCodecs),
+	"source": randomElem(global.lists.sources),
+	"quality": randomElem(global.lists.qualities),
+      }, function(res) {
+	test.equal(res.statusCode, 400);
+	test.done();
+      });
+    }
+  },
+
   rootUser: function(test) {
     request.post('/movie/1/release', global.rootToken, {
       "name": "foobar",
