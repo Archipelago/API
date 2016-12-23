14fdbf1669380fdcccdc98cc1f139ecb6d2b4e20
Niovad <niovad@safe-mail.net>
2016-12-23 01:38:13 +0000

    Fix doc for movie deletion

--- doc/movie.js
+++ doc/movie.js
@@ -189,7 +189,7 @@
  * @apiDescription Delete a movie depending on its id
  * @apiPermission DELETE_ELEMENT
  *
- * @apiParam {Number} id The id of the element that will be to deleted
+ * @apiParam {Number} id The id of the element that will be deleted
  *
  * @apiSuccessExample Success-Response
  *     HTTP/1.1 204 No Content
