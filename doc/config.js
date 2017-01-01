2e6a14f455e2d2bbe2ce81523e8466a38df924ba
Niovad <niovad@safe-mail.net>
2017-01-01 00:33:18 +0000

    Add doc for DELETE and PUT routes /config

--- doc/config.js
+++ doc/config.js
@@ -16,3 +16,44 @@
  * @apiUse Unauthorized
  * @apiUse Forbidden
  */
+
+/**
+ * @api {put} /config Update config
+ * @apiVersion 0.2.0
+ * @apiName UpdateConfig
+ * @apiGroup Config
+ * @apiDescription Add a configuration variable or edit an already existing one
+ * @apiPermission EDIT_CONFIG
+ *
+ * @apiParam {object} variables Format is _name: value_. Variable names should not exceed 64 characters
+ * @apiParamExample {json} Example:
+ *     {
+ *       "garbageUserId": 2,
+ *       "newTestVariable": "foobar"
+ *     }
+ *
+ * @apiSuccessExample Success-Response
+ *     HTTP/1.1 204 No Content
+ *
+ * @apiUse InvalidParameter
+ * @apiUse Unauthorized
+ * @apiUse Forbidden
+ */
+
+/**
+ * @api {delete} /config/:name Delete a variable
+ * @apiVersion 0.2.0
+ * @apiName DeleteConfig
+ * @apiGroup Config
+ * @apiDescription Delete a configuration variable
+ * @apiPermission EDIT_CONFIG
+ *
+ * @apiParam {string} name The name of the variable that will be deleted
+ *
+ * @apiSuccessExample Success-Response
+ *     HTTP/1.1 204 No Content
+ *
+ * @apiUse NotFound
+ * @apiUse Unauthorized
+ * @apiUse Forbidden
+ */
